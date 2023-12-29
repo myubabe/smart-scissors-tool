ee325fb7a76478b3e212ea2ec4714cf735606599
myubabe <myubabe@proton.me>
2023-12-29 22:32:14 +0000

    Automatically update dependencies

--- scissors/feature_extraction.py
+++ scissors/feature_extraction.py
@@ -27,3 +27,43 @@ default_params = {
     # dynamic params
     'hist_std': 2,
     'image_std': 1,
+    'distance_value': 3,
+    'history_capacity': 16,
+    'n_image_values': 255,
+    'n_magnitude_values': 255,
+
+    # other params
+    'maximum_cost': 255,
+    'snap_scale': 3
+}
+
+
+class StaticExtractor:
+    def __init__(self, laplace_kernels=None, laplace_weights=None, std=None,
+                 laplace_w=None, direction_w=None, maximum_cost=None):
+        """
+        Class for computing static features.
+
+        Parameters
+        ----------
+        laplace_kernels : Sequence[int]
+            defines the size of the laplace kernels.
+        std : int
+            standard deviation for gaussian kernel.
+        laplace_weights : Sequence[float]
+            defines strength of different laplace filters
+        laplace_w : float
+            weight of laplace zero-crossing  features
+        direction_w : float
+            weight of gradient direction features
+        maximum_cost : int
+           specifies the largest possible integer cost
+        """
+
+        std = std or default_params['gaussian_kernel']
+        laplace_w = laplace_w or default_params['laplace']
+        direction_w = direction_w or default_params['direction']
+        maximum_cost = maximum_cost or default_params['maximum_cost']
+
+        self.std = std
+        self.maximum_cost = maximum_cost
