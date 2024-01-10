e6ac59ece7aa376280fa4dc7d5fb0632a26c5c96
myubabe <myubabe@proton.me>
2024-01-10 10:41:59 +0000

    Optimization.

--- scissors/gui.py
+++ scissors/gui.py
@@ -24,3 +24,57 @@ class View:
         self.model = model
 
     def update(self):
+        raise NotImplementedError()
+
+    @property
+    def canvas(self):
+        return self.model.canvas
+
+
+class Poly(Model):
+    def __init__(self, canvas):
+        super().__init__(canvas)
+        self.points = []
+
+    def add_point(self, point):
+        self.points.append(point)
+        self.update()
+
+
+class Pixels(Model):
+    def __init__(self, canvas):
+        super().__init__(canvas)
+        self.pixels = []
+
+    def add_pixels(self, pixels):
+        self.pixels.extend(pixels)
+        self.update()
+
+
+class PolyController:
+    def __init__(self, model):
+        self.model = model
+
+    def on_click(self, x, y):
+        self.model.add_point((x, y))
+
+    @property
+    def canvas(self):
+        return self.model.canvas
+
+
+class PixelsView(View):
+    def __init__(self, model, fill_color="yellow"):
+        super().__init__(model)
+        self.fill_color = fill_color
+
+    def update(self):
+        pixels = self.model.pixels
+
+        for pix in pixels:
+            x, y = pix
+            self.canvas.create_rectangle((x, y) * 2, outline=self.fill_color)
+
+
+class PolyView(View):
+    def __init__(self, model, draw_lines=False, fill_color="red", radius=3):
