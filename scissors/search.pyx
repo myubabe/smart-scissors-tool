2d4a49fb431729ea78aa0d39a9d7feff48f68841
myubabe <myubabe@proton.me>
2024-04-09 10:54:32 +0000

    Update api_engine_features.txt

--- scissors/search.pyx
+++ scissors/search.pyx
@@ -176,4 +176,5 @@ def search(long [:, :, :, :]static_cost, long [:, :, :, :] dynamic_cost,
                 # assign neighbor’s cumulative cost
                 set_cost(q, tmp_cost)
                 # place node to the active list
-                list_index = q[0].total_cost % maximum_local_c
+                list_index = q[0].total_cost % maximum_local_cost
+                list_push(q, &active_list[0][l
