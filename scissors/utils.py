730aff60d8a5702ff3bbb873cf116b29765f7318
myubabe <myubabe@proton.me>
2024-04-19 05:32:48 +0000

    node version

--- scissors/utils.py
+++ scissors/utils.py
@@ -4,4 +4,5 @@ from typing import Sequence, Union
 from itertools import product
 
 
-def unfold(x: np.array, filter_size: Union[int, np.array] = 3
+def unfold(x: np.array, filter_size: Union[int, np.array] = 3):
+    feature_size, *spatial = x.shap
