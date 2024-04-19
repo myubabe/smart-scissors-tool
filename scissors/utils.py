928d0f42438bc41b59e563a13c11e509042be9cd
myubabe <myubabe@proton.me>
2024-04-19 12:11:13 +0000

    test?

--- scissors/utils.py
+++ scissors/utils.py
@@ -7,4 +7,6 @@ from itertools import product
 def unfold(x: np.array, filter_size: Union[int, np.array] = 3):
     feature_size, *spatial = x.shape
     if isinstance(filter_size, int):
-        filter_size = 
+        filter_size = np.array((filter_size,) * len(spatial))
+
+    unfol
