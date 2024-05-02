171d9d5caa4930415d1fc3392af6a93aef3d2817
myubabe <myubabe@proton.me>
2024-05-02 18:03:13 +0000

    commit update

--- scissors/utils.py
+++ scissors/utils.py
@@ -37,4 +37,5 @@ def unfold(x: np.array, filter_size: Union[int, np.array] = 3):
     return unfolded
 
 
-def create_spatial_feats(shape: Sequence[int], filter_size: Union[int, np.array] = 3, feature_s
+def create_spatial_feats(shape: Sequence[int], filter_size: Union[int, np.array] = 3, feature_size: int = 2):
+    if isinstance(filter_si
